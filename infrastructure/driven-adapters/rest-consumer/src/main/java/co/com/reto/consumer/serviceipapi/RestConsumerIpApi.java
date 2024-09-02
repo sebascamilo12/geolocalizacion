9e9ffbf7d8e9a95811afa87e5da93d91f304c716
sebascamilo12 <sebaspineda1294@gmail.com>
2024-09-02 00:33:05 -0500

    Se agrega circuitBreaker a RestConsumerIpApi

--- infrastructure/driven-adapters/rest-consumer/src/main/java/co/com/reto/consumer/serviceipapi/RestConsumerIpApi.java
+++ infrastructure/driven-adapters/rest-consumer/src/main/java/co/com/reto/consumer/serviceipapi/RestConsumerIpApi.java
@@ -5,11 +5,13 @@ import co.com.reto.consumer.serviceipapi.ipapi.mapper.UbicacionIpMapper;
 import co.com.reto.model.ubicacionip.UbicacionIp;
 import co.com.reto.model.ubicacionip.gateways.UbicacionIpGateway;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import lombok.RequiredArgsConstructor;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -17,53 +19,28 @@ public class RestConsumerIpApi implements UbicacionIpGateway {
 
     private final WebClient client;
 
+    private static final Logger logger = LoggerFactory.getLogger(RestConsumerIpApi.class);
+
     public RestConsumerIpApi(@Qualifier("ipApiWebClient") WebClient client) {
         this.client = client;
     }
 
-    //    @Value("${adapter.restconsumer.access-key}")
-//    private final String accessKey;
-
-
-    // these methods are an example that illustrates the implementation of WebClient.
-    // You should use the methods that you implement from the Gateway from the domain.
-    @CircuitBreaker(name = "testGet" /*, fallbackMethod = "testGetOk"*/)
-    public Mono<UbicacionIpResponse> testGet() {
-        return client
-                .get()
-                .uri("/{ip}?access_key=a293e059a7f8b6ce3461fa1e3f3c0f46", "125.145")
-                .retrieve()
-                .bodyToMono(UbicacionIpResponse.class);
-    }
-
-// Possible fallback method
-//    public Mono<String> testGetOk(Exception ignored) {
-//        return client
-//                .get() // TODO: change for another endpoint or destination
-//                .retrieve()
-//                .bodyToMono(String.class);
-//    }
-
-//    @CircuitBreaker(name = "testPost")
-//    public Mono<UbicacionIpResponse> testPost() {
-//        ObjectRequest request = ObjectRequest.builder()
-//            .val1("exampleval1")
-//            .val2("exampleval2")
-//            .build();
-//        return client
-//                .post()
-//                .body(Mono.just(request), ObjectRequest.class)
-//                .retrieve()
-//                .bodyToMono(UbicacionIpResponse.class);
-//    }
+    @Value("${adapter.restconsumer-ipapi.access-key}")
+    private String accessKey;
 
     @Override
+    @CircuitBreaker(name = "restConsumer" , fallbackMethod = "fallBackObtenerInformacion")
     public Mono<UbicacionIp> obtenerUbicacionIp(String ip) {
         return client
                 .get()
-                .uri("/{ip}?access_key=a293e059a7f8b6ce3461fa1e3f3c0f46", ip)
+                .uri("/{ip}?access_key="+accessKey, ip)
                 .retrieve()
                 .bodyToMono(UbicacionIpResponse.class)
                 .map(UbicacionIpMapper.MAPPER::restResponseToUbicacionIp);
+    }
+
+    public Mono<UbicacionIp> fallBackObtenerInformacion(String ip, WebClientResponseException ex) {
+        logger.error("Error al obtener la ubicación para la IP: {}. Error: {}", ip, ex.getMessage());
+        return Mono.error(new RuntimeException("Ocurrió un error al obtener la ubicación para la IP: " + ip, ex));
     }
 }
